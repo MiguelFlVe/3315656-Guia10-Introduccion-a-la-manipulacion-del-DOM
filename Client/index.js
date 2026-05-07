9473ae8fadfa3a692ae136f37d1e35521a8239dd
Oscar <mao090997@gmail.com>
2026-05-07 18:29:12 -0500

    feat: se agregan las preguntas al archivo principal del ejercicio

--- Client/index.js
+++ Client/index.js
@@ -1,2 +1,2 @@
-import { isValidInput } from "./Transferencia/TODO1.js"; // TODO1: Implementar validación
+export { isValidInput } from "./Transferencia/TODO1.js"; // TODO1: Implementar validación
 import { createMessageElement } from './TODO9.js';// TODO9: Implementar la creación de un nuevo mensaje
