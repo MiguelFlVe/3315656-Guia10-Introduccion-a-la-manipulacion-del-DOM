94787866e3208feac437f8185f12291e5e728cf1
Oscar <mao090997@gmail.com>
2026-05-07 18:55:02 -0500

    se solucionan conflictos en index.js

--- Client/index.js
+++ Client/index.js
@@ -1,3 +1,2 @@
 export { validateForm } from "./Transferencia/TODO4.js"; // TODO4: Implementar validación completa del formulario
-// TODO9: Implementar la creación de un nuevo mensaje
-import { createMessageElement } from './TODO9.js';
+import { createMessageElement } from './TODO9.js';// TODO9: Implementar la creación de un nuevo mensaje
