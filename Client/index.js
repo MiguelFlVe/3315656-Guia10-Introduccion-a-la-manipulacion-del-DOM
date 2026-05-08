31120b6a3754a39666ff88de08c0561331263a59
Miguel Florez <63000122+MiguelFlVe@users.noreply.github.com>
2026-05-08 12:00:33 -0500

    REFACTOR: Update exports in index.js with TODO comments

--- Client/index.js
+++ Client/index.js
@@ -1,11 +1,11 @@
 export { isValidInput } from "./Transferencia/TODO1.js"; // TODO1: Implementar validación
 
-export { showError } from "./Transferencia/TODO2.js"; 
+export { showError } from "./Transferencia/TODO2.js"; // TODO2: Implementar función para mostrar error
 
 export { clearError } from "./Transferencia/TODO3.js"; // TODO3: Implementar función para limpiar error
 
-export { createMessageElement } from './Transferencia/TODO9.js'; // TODO2: Implementar función para mostrar error
+export { createMessageElement } from './Transferencia/TODO9.js'; // TODO9: Implementar la creación de un nuevo mensaje
 
 export { handleFormSubmit } from './Transferencia/TODO10.js'; // TODO10: Implementar el manejador del evento submit
 
-export { handleInputChange } from './Transferencia/TODO11.js'; // TODO11: Implementar limpieza de errores al escribir // TODO9: Implementar la creación de un nuevo mensaje
+export { handleInputChange } from './Transferencia/TODO11.js'; // TODO11: Implementar limpieza de errores al escribir
